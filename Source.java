37868161f00a4fde327167137f5cac83b9a68835
N1s0 <ninoemery12@gmail.com>
2016-04-15 19:29:50 +0200

    Code without interface
    The code work only if you enter an int value as kWh.

--- Source.java
+++ Source.java
@@ -12,7 +12,7 @@ public class your class {   //write the name of your class
 		System.out.print("Ceci est un convertisseur de KiloWattHeure en bière");
 		System.out.println("\nCombien de kWh voulez-vous convertir ?");
 		int kwh = sc.nextInt();
-		System.out.println("\nCela représente " + kwh*0.15/2.5 + " bière (moinette) !" );
+		System.out.println("\nCela représente " + kwh*0.15/2.5 + " bière (moinette)!" );
 		System.out.println("\nRecommencer ? (o/n)");
 		sc.nextLine();
 		rep = sc.nextLine().charAt(0);
